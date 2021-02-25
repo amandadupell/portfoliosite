61fbd3e4faca2596a86f49d03d699262fd3fde3a
Amanda Dupell <anddupell@gmail.com>
2021-02-24 19:55:43 -0500

    added responsive for footer

--- src/components/footer/constants.ts
+++ src/components/footer/constants.ts
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
+import LinkedInLogo from '../../assets/svgComponents/linkedInComponent';
+import GithubLogo from '../../assets/svgComponents/githubComponent';
+
 export interface Props {
 };
 
 export const Container = styled.div`
     display: flex;
     flex-direction: row;
-    width: 130px;
     justify-content: space-between;
     position: fixed;
 
@@ -16,8 +18,31 @@ export const Container = styled.div`
     padding-bottom: 50px;
     padding-right: 50px;
 
+    width: 130px;
+
     @media screen and (max-width: 800px) {
+        width: 20%;
         padding-bottom: 15px;
         padding-right: 15px;
+    }
+`;
+
+export const StyledLinkedin = styled(LinkedInLogo)`
+    height: 30px;
+    width: 30px;
+
+    @media screen and (max-width: 800px) {
+        height: 20px;
+        width: 20px;
+    }
+`;
+
+export const StyledGithub = styled(GithubLogo)`
+    height: 30px;
+    width: 30px;
+
+    @media screen and (max-width: 800px) {
+        height: 20px;
+        width: 20px;
     }
 `;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,16 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import { Container, Props } from './constants';
-import LinkedInLogo from '../../assets/svgComponents/linkedInComponent';
-import GithubLogo from '../../assets/svgComponents/githubComponent';
+import { Container, Props, StyledLinkedin, StyledGithub } from './constants';
 
 const Footer: FunctionComponent<Props> = () => {
     return (
         <Container>
             <a href={'https://www.linkedin.com/in/amanda-dupell-profile/'} target='_blank' rel="noreferrer">
-                <LinkedInLogo width={25} height={25} />
+                <StyledLinkedin />
             </a>
             <a href={'https://github.com/amandadupell/portfoliosite'} target='_blank' rel="noreferrer">
-                <GithubLogo width={25} height={25} />
+                <StyledGithub />
             </a>
         </Container >
     );
